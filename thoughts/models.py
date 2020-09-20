285d212217388162ae9219944bfaa3b067748fb2
Zelim Anzorov <zelim@anzorov.com>
2020-09-20 20:00:24 +0200

    Add functions to models.

--- thoughts/models.py
+++ thoughts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.urls import reverse
 from scenes.models import Scene
+from characters.models import Character
 
 class Thought(models.Model):
     code = models.CharField(max_length=12, default='')
     scene = models.ForeignKey(Scene, on_delete=models.CASCADE, null=True)
-    character = models.TextField()
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, null=True)
     text = models.TextField()
 
     class Meta:
@@ -19,3 +20,6 @@ class Thought(models.Model):
 
     def get_edit_url(self):
         return reverse("thoughts:edit-thought", kwargs={'pk': self.id})
+
+    def get_delete_url(self):
+        return reverse("thoughts:delete-thought", kwargs={'pk': self.id})
